23b338c0454bae5a89d404819d8cf0d45d5c9d87
Esteban Torres <me@estebantorr.es>
2016-07-11 23:38:29 -0600

    Fixes broken spec

--- spec/danger_plugin_spec.rb
+++ spec/danger_plugin_spec.rb
@@ -33,7 +33,7 @@ module Danger
             proselint_response = '{"status" : "success", "data" : { "errors" : ' + errors + '}}'
 
             # This is where we generate our JSON
-            allow(@prose).to receive(:`).with('proselint spec/fixtures/blog_post.md --json').and_return(proselint_response)
+            allow(@prose).to receive(:`).with('proselint "spec/fixtures/blog_post.md" --json').and_return(proselint_response)
 
             # it's worth noting - you can call anything on your plugin that a Dangerfile responds to
             # The request source's PR JSON typically looks like
